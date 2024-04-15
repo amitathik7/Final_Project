9c520548bed345306c562282eb3ad56dbef8613d
amitathik7 <amitathik7@gmail.com>
2024-04-15 19:31:57 -0400

    Making the code to get the list of unique actor names

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
 import csv
 import pickle
+import time
 from collections import defaultdict
 
 def default_dict():
     return defaultdict(list)
 
-# Initialize an empty dictionary to store the adjacency list
-# adjacency_list = defaultdict(default_dict)
-#
-# # Read the CSV file and parse movie names and associated lists of actor names
-# with open('movies_and_actors.csv', newline='', encoding='utf-8') as csvfile:
-#     reader = csv.reader(csvfile)
-#     next(reader)  # Skip the header row
-#
-#     # Iterate through each row in the CSV file
-#     for row in reader:
-#         movie_name, actors_str = row
-#
-#         # Extract list of actors from the string representation
-#         actors_list = eval(actors_str)
-#
-#         # Update adjacency list based on actors in the current movie
-#         for i in range(len(actors_list)):
-#             for j in range(i + 1, len(actors_list)):
-#                 actor1, actor2 = actors_list[i], actors_list[j]
-#
-#                 # Increment the edge weight between actors
-#                 adjacency_list[actor1][actor2].append(movie_name)
-#                 adjacency_list[actor2][actor1].append(movie_name)
-#
-# with open('movies_and_actors.pickle', 'wb') as f:
-#     pickle.dump(adjacency_list, f)
-
-# print(len(adjacency_list['Will Smith']['Jaden Smith']))
-
-# Print the adjacency list representation of the graph
-# for actor, neighbors in adjacency_list.items():
-#     print(f"{actor}: {neighbors}")
-
 with open("movies_and_actors.pickle", "rb") as f:
     adjacency_list = pickle.load(f)
+
+actor_names = list()
+
+for actors, movies in adjacency_list.items():
+    actor_names.append(actors)
+
+print(actor_names)
