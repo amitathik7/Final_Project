78f2454c95c49f75b7326269a7af9482ea558838
amitathik7 <amitathik7@gmail.com>
2024-04-17 20:49:53 -0400

    Defined both searching functions and results

--- main.py
+++ main.py
@@ -3,17 +3,8 @@ import sys
 import time
 from collections import defaultdict
 
-def default_dict():
-    return defaultdict(list)
-
-start = time.time()
-with open("pickle_files/adjacency_list.pkl", "rb") as f:
-    adjacency_list = pickle.load(f)
-end = time.time()
-
-print(f'Time taken to load adjacency list: {end - start}')
-def dijsktra_search(start_name, end_name):
-    if start_name not in adjacency_list.keys() or end_name not in adjacency_list.keys():
+def dijsktra_search(start_name, end_name, adj_list):
+    if start_name not in adj_list.keys() or end_name not in adj_list.keys():
         return -1, -1
 
     start = time.time()
@@ -21,7 +12,7 @@ def dijsktra_search(start_name, end_name):
     distance = defaultdict(float)
     visited_actors = set()
 
-    for actor in adjacency_list:
+    for actor in adj_list:
         if actor != start_name:
             distance[actor] = sys.maxsize
         else:
@@ -33,7 +24,7 @@ def dijsktra_search(start_name, end_name):
     while (curr_actor != 'invalid'):
         curr_distance = distance[curr_actor]
 
-        for neighbor, movies in adjacency_list[curr_actor].items():
+        for neighbor, movies in adj_list[curr_actor].items():
             neighbor_distance = curr_distance + (1 / len(movies))
             if (distance[neighbor] > neighbor_distance):
                 distance[neighbor] = neighbor_distance
@@ -42,7 +33,7 @@ def dijsktra_search(start_name, end_name):
 
         min = sys.maxsize
 
-        for actor in adjacency_list:
+        for actor in adj_list:
             if distance[actor] < min and actor not in visited_actors:
                 curr_actor = actor
                 min = distance[curr_actor]
@@ -60,5 +51,5 @@ def dijsktra_search(start_name, end_name):
     return final_score, time_taken
 
 
-def bellman_ford_search(start_name, end_name):
-    return -1
+def bellman_ford_search(start_name, end_name, adj_list):
+    return -1, -1
